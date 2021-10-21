059d2343cbbeb15f4e8552f4062e3bf5810fac3c
ingjavierbarrios <42646753+ingjavierbarrios@users.noreply.github.com>
2021-10-21 16:19:22 -0300

    Update validate.js

--- day1/validate.js
+++ day1/validate.js
@@ -1,5 +1,3 @@
 function myFunction() {
-      if (document.getElementByID("password").value == "sr2578")
-        document.getElementByID("resultado").innerHTML = "OK";
-      else document.getElementByID("resultado").innerHTML = "Wrong Password. Try Again!";
-    }
+        alert("Hola!!!");
+      }
